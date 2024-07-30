d7e33e4c3a0ff3ed5b9afb874b068acfd504174a
Elina <elinaaman0302@gmail.com>
2024-07-30 13:00:44 +0100

    Initial Commit

---
+++ resultpage2/resultpage2App.swift
@@ -0,0 +1,17 @@
+//
+//  resultpage2App.swift
+//  resultpage2
+//
+//  Created by Scholar on 30/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct resultpage2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
