c7e8211e92cb89568d8826e203534d93c302133d
Elina <elinaaman0302@gmail.com>
2024-07-30 15:26:03 +0100

    This is the results page

--- resultpage2/ContentView.swift
+++ resultpage2/ContentView.swift
@@ -9,13 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            Color("purple1")
+                .ignoresSafeArea()
+           
+            VStack {
+               
+                    
+               
+                
+                Image("cat2")
+                
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15.0)
+                    .padding(.horizontal, 20.0)
+                Text("")
+            }
+                
+                
+                
+                    .frame(width: 360.0, height: 600.0)
+                    .background(Rectangle())
+                    .foregroundColor(.white)
+                    .cornerRadius(15.0)
+            Text("")
+            
+            Text("The Perfectionist Procrastinator")
+                .font(.title3)
+            
+                .fontWeight(.heavy)
+                .padding(.bottom, 450.0)
         }
-        .padding()
     }
 }
 
